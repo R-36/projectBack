0a25b32e3556a2a3cbc87e7ee4cefc4c82df5896
Ivan Politov <withoutenemy@mail.ru>
2020-03-21 03:11:05 +0300

    add registraton and login modules

--- Start.py
+++ Start.py
@@ -1,5 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:123@localhost/py_hack'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+db.create_all()
 
 
 @app.route('/')
@@ -7,5 +29,41 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = request.get_json(force=True)
+    if User.query.filter_by(username=data['nickname']).first() is not None:
+        data = {'status': 'Failed',
+                'message': 'Nickname is already used'}
+        return jsonify(data), 400
+    if User.query.filter_by(email=data['email']).first() is not None:
+        data = {'status': 'Failed',
+                'message': 'Nickname is already used'}
+        return jsonify(data), 400
+    else:
+        user = User(username=data['nickname'], email=data['email'], password=data['password'], status='user')
+        db.session.add(user)
+        db.session.commit()
+        data = {'status': 'Success'}
+        return jsonify(data), 200
+
+
+@app.route('/login_user', methods=['POST'])
+def login_user():
+    data = request.get_json(force=True)
+    user = User.query.filter_by(email=data['email']).first()
+    if user is not None:
+        if user.password == data['password']:
+            data = {'status': 'Success'}
+            return jsonify(data), 200
+        else:
+            data = {'status': 'Failed',
+                    'message': 'Password is incorrect'}
+            return jsonify(data), 400
+    data = {'status': 'Failed',
+            'message': 'User not found'}
+    return jsonify(data), 400
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,6 +1,6 @@
-#!C:\Users\Witho\PycharmProjects\hack\venv\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
-__requires__ = 'setuptools==40.8.0'
+#!c:\users\witho\pycharmprojects\hack\venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==46.0.0','console_scripts','easy_install'
+__requires__ = 'setuptools==46.0.0'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+        load_entry_point('setuptools==46.0.0', 'console_scripts', 'easy_install')()
     )
